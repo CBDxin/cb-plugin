0552f418505826784bc78b431e1fab65b662bbab
liucanbin <liucanbin@cvte.com>
2020-10-08 22:19:27 +0800

    [feat]增加import less的补全

--- src/classCompletion/index.ts
+++ src/classCompletion/index.ts
@@ -1,21 +1,32 @@
 import * as vscode from 'vscode';
 import findCssClassNames from '../utli/findCssClassNames';
 import getPath from '../utli/getPath';
+import * as path from 'path'; 
 
 const classMatchReg = /className=["|']([\w- ]*$)/;
+const importMatchReg = /import[\s]*["|']([^:\s;]+)(\.less)["|']/g;
 
 const provideCompletionItems = async(document: vscode.TextDocument, position: vscode.Position) => {
   const start: vscode.Position = new vscode.Position(position.line, 0);
   const range: vscode.Range = new vscode.Range(start, position);
   const text: string = document.getText(range);
+  const content: string = document.getText();
   const globalCssPath = await getPath.getGlobalCssPath();
+  let classNames:string[] = [], importMatches;
 
   const rawClasses = text.match(classMatchReg);
 	if (!rawClasses || rawClasses.length === 1 || globalCssPath === '') {
 		return [];
 	}
 
-  const classNames = await findCssClassNames(globalCssPath);
+  while ((importMatches = importMatchReg.exec(content)) !== null ) {
+    let importPath = importMatches[1] + importMatches[2];
+    let importClassNames = await findCssClassNames(path.resolve(path.dirname(document.fileName), importPath));
+    classNames = [...classNames, ...importClassNames];
+  }
+
+  let globalClassNames = globalCssPath ? await findCssClassNames(globalCssPath) : [];
+  classNames = [...classNames, ...globalClassNames];
 
   return classNames.map((className) => {
     const completionItem = new vscode.CompletionItem(className, vscode.CompletionItemKind.Variable);
